1db279fa626672f217b70cbea7738c12a6a6be9a
yuandong <888@jzy.com>
2020-05-23 19:57:34 +0800

    .

--- app/src/main/java/com/yuong/running/ui/activity/SportMapActivity.java
+++ app/src/main/java/com/yuong/running/ui/activity/SportMapActivity.java
@@ -1,5 +1,6 @@
 package com.yuong.running.ui.activity;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -12,17 +13,26 @@ import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.Polyline;
+import com.amap.api.maps.model.PolylineOptions;
+import com.amap.api.maps.model.animation.Animation;
+import com.amap.api.maps.model.animation.ScaleAnimation;
 import com.yuong.running.R;
 import com.yuong.running.common.activity.BaseActivity;
 import com.yuong.running.common.utils.LogUtils;
 import com.yuong.running.eventbus.LocationEvent;
-import com.yuong.running.utils.LocationUtils;
+import com.yuong.running.eventbus.UpdatePathEvent;
+import com.yuong.running.utils.PathSmoothTool;
 import com.yuong.running.utils.SportPathHelper;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.List;
 
 /**
  * 地图运动轨迹
@@ -34,9 +44,12 @@ public class SportMapActivity extends BaseActivity implements LocationSource {
 
     private AMap aMap;
     private SportPathHelper mSportLocationUtils;
-    private LocationUtils mLocationUtils;
     private OnLocationChangedListener mListener;
     private boolean mFirstFix = false;
+    private boolean mFirstDraw = true;
+    private PathSmoothTool mPathSmoothTool;
+    private Polyline mPolyline;
+    private PolylineOptions polylineOptions;
 
     @Override
     public int getLayoutId() {
@@ -59,9 +72,11 @@ public class SportMapActivity extends BaseActivity implements LocationSource {
     public void initData(Bundle savedInstanceState) {
         EventBus.getDefault().register(this);
         mSportLocationUtils = SportPathHelper.getInstance();
-        mLocationUtils = LocationUtils.getInstance();
+        mPathSmoothTool = new PathSmoothTool();
+        mPathSmoothTool.setIntensity(4);
         mapView.onCreate(savedInstanceState);// 此方法必须重写
         initMap();
+        initPolyline();
     }
 
     private void initMap() {
@@ -86,9 +101,18 @@ public class SportMapActivity extends BaseActivity implements LocationSource {
         aMap.setMyLocationEnabled(true);
     }
 
+    private void initPolyline() {
+        polylineOptions = new PolylineOptions();
+        polylineOptions.color(getResources().getColor(R.color.color_49B557));
+        polylineOptions.width(20f);
+        polylineOptions.lineCapType(PolylineOptions.LineCapType.LineCapRound);
+        polylineOptions.lineJoinType(PolylineOptions.LineJoinType.LineJoinRound);
+        polylineOptions.useGradient(true);
+        polylineOptions.geodesic(true);
+    }
+
     @Override
     public void activate(OnLocationChangedListener onLocationChangedListener) {
-        mFirstFix = false;
         mListener = onLocationChangedListener;
     }
 
@@ -130,14 +154,46 @@ public class SportMapActivity extends BaseActivity implements LocationSource {
         AMapLocation location = event.getaMapLocation();
         LogUtils.i(TAG, "longitude : " + location.getLongitude());
         LogUtils.i(TAG, "latitude : " + location.getLatitude());
-
+        mListener.onLocationChanged(location);// 显示系统小蓝点
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         if (!mFirstFix) {
             mFirstFix = true;
-            mListener.onLocationChanged(location);// 显示系统小蓝点
             aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+            List<LatLng> existLatLngs = mSportLocationUtils.getPathRecord().getPathLinePoints();
+            //绘制起点
+            addStartMarker(existLatLngs.get(0));
+
+            //绘制轨迹
+            polylineOptions.addAll(existLatLngs);
+            aMap.addPolyline(polylineOptions);
         } else {
             aMap.animateCamera(CameraUpdateFactory.changeLatLng(latLng));
         }
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void updatePathEvent(UpdatePathEvent event) {
+        LogUtils.i(TAG, "刷新地图轨迹.....");
+        //TODO
+        List<LatLng> originList = mSportLocationUtils.getPathRecord().getPathLinePoints();
+//        List<LatLng> optimizeList = mPathSmoothTool.pathOptimize(originList);
+        List<LatLng> latLngs = originList;
+        if (latLngs != null && latLngs.size() > 0 && mFirstFix) {
+            LatLng curLatLng = latLngs.get(latLngs.size() - 1);
+            polylineOptions.add(curLatLng);
+            aMap.addPolyline(polylineOptions);
+        }
+    }
+
+    private void addStartMarker(LatLng latLng) {
+        MarkerOptions options = new MarkerOptions();
+        options.position(latLng);
+        options.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(this.getResources(), R.mipmap.icon_start_mark)));
+        Marker marker = aMap.addMarker(options);
+        Animation markerAnimation = new ScaleAnimation(0, 1, 0, 1); //初始化生长效果动画
+        markerAnimation.setDuration(1000);  //设置动画时间 单位毫秒
+        marker.setAnimation(markerAnimation);
     }
 }
